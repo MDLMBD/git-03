daaee0439a89818e03b7ccde66dec87aba5cbab9
Neri Valdez <20253ds041@utez.edu.mx>
2026-05-22 14:51:18 -0600

    [NEW] constructor nombre y edad

--- src/main/java/org/example/Neri.java
+++ src/main/java/org/example/Neri.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Neri {
+    private String name;
+    private int age;
+    public Neri(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }

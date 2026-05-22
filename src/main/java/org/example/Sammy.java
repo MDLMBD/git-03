b14aee9bc8e1f698b5eeec1e2fd4d8673c703840
Samantha Terrones <20253ds057@utez.edu.mx>
2026-05-22 15:03:03 -0600

    [NEW]cambios nuevos

--- src/main/java/org/example/Sammy.java
+++ src/main/java/org/example/Sammy.java
@@ -4,10 +4,4 @@ public class Sammy {
 private  String name;
 private String surname;
 private int age;
-
-public Sammy(String name, String surname, int age) {
-    this.name=name;
-    this.surname=surname;
-    this.age=age;
-}
 }

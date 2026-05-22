7ab995ec18b2f86e94b022eb0abb496d2084fa0c
Mateo Quintero <20253ds054@utez.edu.mx>
2026-05-22 14:51:29 -0600

    [NEW] Metodo constructor

--- src/main/java/org/example/Mateo.java
+++ src/main/java/org/example/Mateo.java
@@ -5,4 +5,9 @@ public class Mateo {
     private String nombre;
     private String apellido;
 
+    public Mateo(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
 }

b645ed289d7cb5a83679ccbfb9c1fb1a1f50e39d
Allisson Dominguez <20253ds053@utez.edu.mx>
2026-05-22 15:04:55 -0600

    [NEW] metodo

--- src/main/java/org/example/Sugey.java
+++ src/main/java/org/example/Sugey.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Sugey {
+    private String name;
+    private int age;
+
+    public Sugey(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
 }

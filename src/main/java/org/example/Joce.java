6fbec8d2bf8ab6ca09e4ca6906b555e81c3a58bd
Jocelyn Martinez <20253ds048@utez.edu.mx>
2026-05-22 14:59:39 -0600

    [NEW] primer commit

--- src/main/java/org/example/Joce.java
+++ src/main/java/org/example/Joce.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Joce {
+
+    private String nombre;
+    private int edad;
+
+    public Joce(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 }

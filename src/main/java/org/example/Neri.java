00e8cd0df97e27a59ad585fb79d779043fb431aa
Neri Valdez <20253ds041@utez.edu.mx>
2026-05-22 14:56:04 -0600

    [FIX]agregar variables

--- src/main/java/org/example/Neri.java
+++ src/main/java/org/example/Neri.java
@@ -3,8 +3,10 @@ package org.example;
 public class Neri {
     private String name;
     private int age;
+    private double peso;
     public Neri(String name, int age) {
         this.name = name;
         this.age = age;
+        this.peso = 0;
     }
 }

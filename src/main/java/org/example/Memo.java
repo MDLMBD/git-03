7b828433dfde9c2eb857c8b3b4df43e36070a0d3
Angel Murga <20233tn150@utez.edu.mx>
2026-05-22 14:51:41 -0600

    [NEW] clase memo 2

--- src/main/java/org/example/Memo.java
+++ src/main/java/org/example/Memo.java
@@ -3,8 +3,13 @@ package org.example;
 public class Memo {
 
     private String memo;
+    private String memo2;
 
     public Memo(String memo) {
         this.memo = memo;
+    }
+
+    public Memo(){
+        this.memo2 = memo2;
     }
 }

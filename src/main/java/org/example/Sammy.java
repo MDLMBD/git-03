1d8bc2d0e5e8ad9a8439923a3b0daa613037d7a4
Samantha Terrones <20253ds057@utez.edu.mx>
2026-05-22 14:54:25 -0600

    [NEW]nueva persona

--- src/main/java/org/example/Sammy.java
+++ src/main/java/org/example/Sammy.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Sammy {
+private  String name;
+private String surname;
+private int age;
+
+public Sammy(String name, String surname, int age) {
+    this.name=name;
+    this.surname=surname;
+    this.age=age;
+}
 }
